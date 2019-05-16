87c6ea699df7e484a7e80c4f8c404ba8b59c9ce1
aksenov1992 <mr.a.aksenov@gmail.com>
2019-05-16 19:47:02 +0700

    сделал js

--- source/js/app.js
+++ source/js/app.js
@@ -1,19 +1,20 @@
   var navMain = document.querySelectorAll('.list-site__element--closed');
   var navToggle = document.querySelector('.main-nav__toggle');
 
+navMain.forEach(function (nojs) {
+  if (nojs.classList.contains('list-site__element--nojs')) {
+    nojs.classList.remove('list-site__element--nojs');
+  }
+});
 
-  navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
+navToggle.addEventListener('click', function() {
+  navMain.forEach(function (items) {
+    if (items.classList.contains('list-site__element--closed')) {
+      items.classList.remove('list-site__element--closed');
+      items.classList.add('list-site__element--opened');
     } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
+      items.classList.add('list-site__element--closed');
+      items.classList.remove('list-site__element--opened');
     }
   });
-
-
-  navMain.forEach() {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
-}
+});
