23a1a393825550034b487ddd017e93735a5f7797
aksenov1992 <mr.a.aksenov@gmail.com>
2019-05-17 01:41:52 +0700

    готово

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,9 @@
   var navMain = document.querySelectorAll('.list-site__element--closed');
   var navToggle = document.querySelector('.main-nav__toggle');
+  var popupWeekOrder = document.querySelector('.product-week__buy');
+  var popupBasketOrder = document.querySelector('.product-card__basket');
+  var popupShow = document.querySelector('.modal-basket');
+  var popupOverlay = document.querySelector('.modal-basket__overlay');
 
 navMain.forEach(function (nojs) {
   if (nojs.classList.contains('list-site__element--nojs')) {
@@ -17,4 +21,32 @@ navToggle.addEventListener('click', function() {
       items.classList.remove('list-site__element--opened');
     }
   });
+
+  if (navToggle.classList.contains('main-nav__toggle--opened')) {
+    navToggle.classList.remove('main-nav__toggle--opened');
+  }
+  else {
+    navToggle.classList.add('main-nav__toggle--opened');
+  }
+});
+
+if (null !== popupWeekOrder) {
+  popupWeekOrder.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    popupShow.classList.add('modal-basket--opened');
+    popupOverlay.classList.add('modal-basket__overlay--show');
+  });
+}
+
+if (null !== popupBasketOrder) {
+  popupBasketOrder.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    popupShow.classList.add('modal-basket--opened');
+    popupOverlay.classList.add('modal-basket__overlay--show');
+  });
+}
+
+popupOverlay.addEventListener('click', function() {
+  popupShow.classList.remove('modal-basket--opened');
+  popupOverlay.classList.remove('modal-basket__overlay--show');
 });
